f74824b7c6be955d6cf695846bac303f7dfd8c6c
Nguyen Minh Vuong <cauvongtrongdembt@gmail.com>
2020-08-28 22:47:12 +0700

    v 1.0.3
    Fix default function value

--- src/veditor.vue
+++ src/veditor.vue
@@ -23,22 +23,27 @@ export default {
     //  disabled status
     disabled: { type: Boolean, default: false },
     //  Toolbar needed
-    toolbar: { type: Array, default: [
-      ['bold', 'italic', 'underline', 'strike'],
-      ['blockquote', 'code-block'],
-      [{ 'header': 1 }, { 'header': 2 }],
-      [{ 'list': 'ordered' }, { 'list': 'bullet' }],
-      [{ 'script': 'sub' }, { 'script': 'super' }],
-      [{ 'indent': '-1' }, { 'indent': '+1' }],
-      [{ 'direction': 'rtl' }],
-      [{ 'size': ['small', false, 'large', 'huge'] }],
-      [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-      [{ 'color': [] }, { 'background': [] }],
-      [{ 'font': [] }],
-      [{ 'align': [] }],
-      ['clean'],
-      ['link', 'image', 'video']
-    ], },
+    toolbar: {
+      type: Array,
+      default: function() {
+        return [
+          ['bold', 'italic', 'underline', 'strike'],
+          ['blockquote', 'code-block'],
+          [{ 'header': 1 }, { 'header': 2 }],
+          [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+          [{ 'script': 'sub' }, { 'script': 'super' }],
+          [{ 'indent': '-1' }, { 'indent': '+1' }],
+          [{ 'direction': 'rtl' }],
+          [{ 'size': ['small', false, 'large', 'huge'] }],
+          [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+          [{ 'color': [] }, { 'background': [] }],
+          [{ 'font': [] }],
+          [{ 'align': [] }],
+          ['clean'],
+          ['link', 'image', 'video']
+        ]
+      },
+    },
     //  Textarea placeholder
     placeholder: { type: String, default: 'Type your text content...' },
     //  Themes of Quill
